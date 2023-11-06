5d8f7fc6662373f5e4bb5d2e6051d79a323c1dfd
권상구 <milennium9@startergames.com>
2023-11-06 17:29:53 +0900

    TimerAtFixedTimeInterval 버그 수정
    Interval이 24시에 딱 떨어지는 경우 0시 초기화가 일어나지 않는 버그 수정.

--- NetProject/Timers/TimerAtFixedTimeInterval.cs
+++ NetProject/Timers/TimerAtFixedTimeInterval.cs
@@ -2,18 +2,27 @@ using System;
 
 namespace GameTimer {
     public class TimerAtFixedTimeInterval : TimerBase {
-
         public TimeSpan timeInterval { get; }
-        
+
         public TimerAtFixedTimeInterval(IDateTimeProvider dateTimeProvider, TimeSpan timeInterval) : base(dateTimeProvider) {
+            if (timeInterval >= TimeSpan.FromDays(1))
+                throw new ArgumentException("timeInterval must be less than 1 day");
+
             this.timeInterval = timeInterval;
         }
 
         protected override DateTime _GetNextTime(DateTime lastTime) {
-            var resetTime = DateTimeProvider.Now.Date + timeInterval;
+            var yesterday     = DateTimeProvider.Now.Date.AddDays(-1);
+            var resetTime = yesterday + timeInterval;
             while (resetTime <= lastTime) {
+                if (resetTime + timeInterval > yesterday.AddDays(1)) {
+                    resetTime = yesterday.AddDays(1);
+                    yesterday = resetTime;
+                }
+
                 resetTime += timeInterval;
             }
+
             return resetTime;
         }
     }
