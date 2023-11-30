2f1475d8ab2db9583455632ca9ae2464e868121a
권상구 <milennium9@startergames.com>
2023-11-30 17:49:16 +0900

    UtcDateTimeProvider 추가.

---
+++ NetProject/DateTimeProviders/UtcDateTimeProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Starter.GameTimer.Interfaces;
+
+namespace Starter.GameTimer.DateTimeProviders {
+    public class UtcDateTimeProvider : IDateTimeProvider {
+        public DateTime     Now          => DateTime.UtcNow;
+        public DateTime     UtcNow       => DateTime.UtcNow;
+        public TimeZoneInfo TimeZoneInfo => TimeZoneInfo.Utc;
+    }
+}
